6380bc9aae93024d330feb20c7a937babdd9c92c
Yash Kaushik <kaushikyash262@gmail.com>
2025-06-02 23:56:01 +0530

    Setup Cookies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import {UserContextProvider} from './context/userContext';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
@@ -7,11 +8,13 @@ import Home from './pages/Home';
 const App = () => {
   return (
     <div>
-      <Routes>
+      <UserContextProvider>
+        <Routes>
         <Route path='/' exact element={<Home />} />
         <Route path='/login' element={<Login />}/>
         <Route path='/signup' element={<Signup />}/>
       </Routes>
+      </UserContextProvider>
     </div>
   )
 }
